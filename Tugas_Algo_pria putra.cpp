ca35150962a8345bc4312fc6f10604bbed7402d7
PriaPutraPartama <122553496+PriaPutraPartama@users.noreply.github.com>
2023-01-16 14:53:29 +0700

    Add files via upload

---
+++ Tugas_Algo_pria putra.cpp
@@ -0,0 +1,21 @@
+//Nama : Pria Putra Partama Harahap
+//Npm : 227064516087
+#include<iostream>
+using namespace std;
+
+typedef unsigned short int ushort;
+typedef signed short int sigshort;
+typedef long double angkapecahan;
+
+int main(){
+	
+	ushort a =666;
+	sigshort b=-666;
+	angkapecahan c=15.5;
+	
+	cout << a <<endl;
+	cout << b <<endl;
+	cout << c <<endl;
+	
+	return 0;
+}
